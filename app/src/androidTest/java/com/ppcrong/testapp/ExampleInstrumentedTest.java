dac6282f0058ce121861a686773285ce9788a08a
ppcrong <ppcrong@gmail.com>
2017-02-17 18:02:01 +0800

    Change appid to com.ppcrong.testapp

--- app/src/androidTest/java/com/ppcrong/testapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ppcrong/testapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("deng.yr.testapp", appContext.getPackageName());
+        assertEquals("com.ppcrong.testapp", appContext.getPackageName());
     }
 }
