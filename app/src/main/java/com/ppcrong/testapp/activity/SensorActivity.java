b2cd78dc6366990b9f7c234402dcec4402559b92
ppcrong <ppcrong@gmail.com>
2018-10-01 17:44:54 +0800

    [ppcrong.20181001][TESTAPP] Force each activity portrait and add logging A/G/M sensors data

--- app/src/main/java/com/ppcrong/testapp/activity/SensorActivity.java
+++ app/src/main/java/com/ppcrong/testapp/activity/SensorActivity.java
@@ -2,6 +2,8 @@ package com.ppcrong.testapp.activity;
 
 import android.Manifest;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,24 +21,37 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class SensorActivity extends AppCompatActivity {
+public class SensorActivity extends AppCompatActivity implements SensorEventListener {
 
     // region [Variable]
     SensorManager mSensorManager;
+    private final float[] mAccelerometerReading = new float[3];
+    private final float[] mGyroscopeReading = new float[3];
+    private final float[] mMagnetometerReading = new float[3];
+
+    private final float[] mRotationMatrix = new float[9];
+    private final float[] mOrientationAngles = new float[3];
     // endregion [Variable]
 
     // region [Widget]
     @BindView(R.id.btn_list_sensors)
     Button mBtnListSensors;
+    @BindView(R.id.btn_start_log)
+    Button mBtnStartLog;
     // endregion [Widget]
 
     // region [OnClick]
     @OnClick(R.id.btn_list_sensors)
     public void onClickBtnListSensors() {
 
+        if (mSensorManager == null) {
+
+            KLog.i("mSensorManager is null...");
+            return;
+        }
+
         KLog.i();
 
-        mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
         final List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         KLog.i("=== LIST AVAILABLE SENSORS ===");
         KLog.i(String.format(Locale.getDefault(), "|%-40s|%-50s|%-10s|", "SensorName", "StringType", "Type"));
@@ -46,6 +61,11 @@ public class SensorActivity extends AppCompatActivity {
 
         KLog.i("=== LIST AVAILABLE SENSORS ===");
     }
+
+    @OnClick(R.id.btn_list_sensors)
+    public void onClickBtnStartLog() {
+
+    }
     // endregion [OnClick]
 
     // region [Life Cycle]
@@ -54,9 +74,93 @@ public class SensorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         KLog.i();
 
+        initSensor();
         initView();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Get updates from the accelerometer and magnetometer at a constant rate.
+        // To make batch operations more efficient and reduce power consumption,
+        // provide support for delaying updates to the application.
+        //
+        // In this example, the sensor reporting delay is small enough such that
+        // the application receives an update before the system checks the sensor
+        // readings again.
+        Sensor accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometer != null) {
+            mSensorManager.registerListener(this, accelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+        Sensor gyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if (gyroscope != null) {
+            mSensorManager.registerListener(this, gyroscope,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+        Sensor magneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (magneticField != null) {
+            mSensorManager.registerListener(this, magneticField,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Don't receive any more updates from either sensor.
+        mSensorManager.unregisterListener(this);
+    }
     // endregion [Life Cycle]
+
+    // region [Sensor]
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            System.arraycopy(event.values, 0, mAccelerometerReading,
+                    0, mAccelerometerReading.length);
+            KLog.i(String.format(Locale.getDefault(), "A,%d,%10.3f,%10.3f,%10.3f", event.timestamp,
+                    event.values[0], event.values[1], event.values[2]));
+        } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+
+            System.arraycopy(event.values, 0, mGyroscopeReading,
+                    0, mGyroscopeReading.length);
+            KLog.i(String.format(Locale.getDefault(), "G,%d,%10.3f,%10.3f,%10.3f", event.timestamp,
+                    event.values[0], event.values[1], event.values[2]));
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+
+            System.arraycopy(event.values, 0, mMagnetometerReading,
+                    0, mMagnetometerReading.length);
+            KLog.i(String.format(Locale.getDefault(), "M,%d,%10.3f,%10.3f,%10.3f", event.timestamp,
+                    event.values[0], event.values[1], event.values[2]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+        // You must implement this callback in your code.
+    }
+
+    // Compute the three orientation angles based on the most recent readings from
+    // the device's accelerometer and magnetometer.
+    public void updateOrientationAngles() {
+
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(mRotationMatrix, null,
+                mAccelerometerReading, mMagnetometerReading);
+
+        // "mRotationMatrix" now has up-to-date information.
+
+        SensorManager.getOrientation(mRotationMatrix, mOrientationAngles);
+
+        // "mOrientationAngles" now has up-to-date information.
+    }
+    // endregion [Sensor]
 
     // region [Private Function]
     private void initView() {
@@ -70,6 +174,11 @@ public class SensorActivity extends AppCompatActivity {
 
             KLog.i("BODY_SENSORS granted");
         }, Manifest.permission.BODY_SENSORS);
+    }
+
+    private void initSensor() {
+
+        mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
     }
     // endregion [Private Function]
 }
